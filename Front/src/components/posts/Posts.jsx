0e294157449f5d66fe7d955991481953121bb5b3
anuragK-24 <24sept.anurag@gmail.com>
2023-09-10 14:32:13 +0530

    Added the search bar functionality

--- Front/src/components/posts/Posts.jsx
+++ Front/src/components/posts/Posts.jsx
@@ -4,6 +4,7 @@ import Post from '../post/Post'
 export default function Posts({posts}) {
   return (
     <div className='posts'>
+      {posts.length===0 &&  <h1 className='text'>No Blog bound</h1>}
       {posts.map( (p) =>( 
           <Post post={p}/>
       ))}
--- Front/src/components/topbar/TopBar.jsx
+++ Front/src/components/topbar/TopBar.jsx
@@ -1,49 +1,86 @@
-import  './topbar.css'
-import { Link } from 'react-router-dom';
-import { useContext } from "react";
-import { Context } from "../../context/Context"
+import "./topbar.css";
+import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Context } from "../../context/Context";
 
-export default function TopBar() {  
-  const { user, dispatch } = useContext(Context) ;
+export default function TopBar() {
+  const { user, dispatch } = useContext(Context);
 
-    const handleLogout = () =>{
-        dispatch({type:"LOGOUT"})
-    }
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
+  const [userPost, setUserPost] = useState("");
 
   return (
     <div className="top">
-        <div className="topLeft">
-            <a href=''><i className="topIcon fa-brands fa-facebook"></i></a>
-            <i className="topIcon fa-brands fa-instagram"></i>
-            <a href='https://twitter.com/AnuragS41695054'><i className="topIcon fa-brands fa-twitter"></i></a>
-            <i className="topIcon fa-brands fa-pinterest"></i>
-        </div>
-        <div className="topCentre">
-            <ul className='topList'>
-                <li className='topListItem'><Link className='link' to="/" >Home</Link></li>
-                <li className='topListItem'><Link className='link' to="/" >About</Link></li>
-                <li className='topListItem'><Link className='link' to="/" >Contact</Link></li>
-                <li className='topListItem'><Link className='link' to="/Write" >Write</Link></li>
-                <li  className='topListItem' onClick={handleLogout}>{user && "Logout"}</li>
-            </ul>
-            
-        </div>
-        <div className="topRight">
-            
-            { user ? (
-                <Link to="/settings"> <img className='topImage' src={user.profilePic} alt="" /> </Link>
-            ) : (<ul className="topList">
-                    <li className="topListItem">
-                        <Link className='link' to="/login">Login</Link>
-                    </li>
-                    <li className="topListItem">
-                        <Link className='link' to="/register">Register</Link>
-                    </li>
-                 </ul>)
-            }
-            
-            <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
-        </div>
+      <div className="topLeft">
+        <a href="">
+          <i className="topIcon fa-brands fa-facebook"></i>
+        </a>
+        <i className="topIcon fa-brands fa-instagram"></i>
+        <a href="https://twitter.com/AnuragS41695054">
+          <i className="topIcon fa-brands fa-twitter"></i>
+        </a>
+        <i className="topIcon fa-brands fa-pinterest"></i>
+      </div>
+      <div className="topCentre">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              About
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              Contact
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/Write">
+              Write
+            </Link>
+          </li>
+          <li className="topListItem" onClick={handleLogout}>
+            {user && "Logout"}
+          </li>
+        </ul>
+      </div>
+      <div className="topRight">
+        {user ? (
+          <Link to="/settings">
+            {" "}
+            <img className="topImage" src={user.profilePic} alt="" />{" "}
+          </Link>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                Register
+              </Link>
+            </li>
+          </ul>
+        )}
+        <input
+          className="searchBox"
+          type="text"
+          onChange={(e) => {
+            setUserPost(e.target.value);
+          }}
+        />
+        <Link to={`/?user=${userPost}`} className="link">
+          <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
